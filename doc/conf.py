53a5e87f4c9e41cd54156dbd5da27528145acb62
Langston Barrett <langston.barrett@gmail.com>
2022-08-22 12:42:56 -0400

    doc: Skeleton

---
+++ doc/conf.py
@@ -0,0 +1,9 @@
+# Configuration file for the Sphinx documentation builder.
+project = 'czz'
+copyright = '2022, Langston Barrett'
+author = 'Langston Barrett'
+extensions = []
+templates_path = []
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+html_theme = 'alabaster'
+html_static_path = []
