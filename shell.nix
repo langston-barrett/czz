739167dc1bd51ec0e0063d670fbfb920ed0a5fa9
Langston Barrett <langston.barrett@gmail.com>
2022-08-25 08:33:59 -0400

    shell.nix: init

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ ghc ? "ghc921"
+, unstableHaskell ? false
+}:
+
+let
+  pkgs = import <nixpkgs> { };
+  unstable = import <unstable> { };
+  hPkgs = if unstableHaskell then unstable else pkgs;
+in pkgs.mkShell {
+  shellHook = ''
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.zlib}/lib
+  '';
+  buildInputs =  [
+    pkgs.clang_11
+    pkgs.llvm_11
+    pkgs.z3
+
+    (hPkgs.haskell.packages.${ghc}.ghcWithPackages (hpkgs: with hpkgs; []))
+    hPkgs.haskell-language-server
+    hPkgs.haskellPackages.ghcid
+    hPkgs.haskellPackages.hlint
+    hPkgs.haskellPackages.cabal-install
+    pkgs.zlib  # needed for Haskell
+  ];
+}
